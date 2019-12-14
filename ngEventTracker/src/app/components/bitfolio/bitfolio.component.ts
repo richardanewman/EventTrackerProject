ab4465473a762553fd21aeacbcc83b5174dd681b
Richard Newman <17536735+richardanewman@users.noreply.github.com>
2019-12-13 18:59:14 -0700

    stubs components

---
+++ ngEventTracker/src/app/components/bitfolio/bitfolio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bitfolio',
+  templateUrl: './bitfolio.component.html',
+  styleUrls: ['./bitfolio.component.css']
+})
+export class BitfolioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
